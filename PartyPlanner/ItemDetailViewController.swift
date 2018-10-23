20c7434fb1fe04e80a5acd3054b8884a605b588e
John Gallaugher <john.gallaugher@bc.edu>
2018-10-23 16:54:47 -0400

    I hope this works

---
+++ PartyPlanner/ItemDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ItemDetailViewController.swift
+//  PartyPlanner
+//
+//  Created by John Gallaugher on 10/16/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
